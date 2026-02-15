c9f39fde59f692d8ebcfc864ec24fe28d41131a9
Sakthivelamari <sakthivelmaribe@gmail.com>
2026-02-15 19:24:30 +0530

    properties added

--- Habit_Tracker/Habit_Tracker/Models/Habit.cs
+++ Habit_Tracker/Habit_Tracker/Models/Habit.cs
@@ -2,5 +2,11 @@
 {
     public class Habit
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<DateTime> CompletedDates { get; set; } = new List<DateTime>();
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
     }
 }
